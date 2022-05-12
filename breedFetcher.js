961a8b0fbc9f3c6d61ff6e9e3c1e2d46bdee72e1
ESDUNCAN <d.eric@rogers.com>
2022-05-12 20:54:33 +0000

    update

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,20 +1,27 @@
-const breed = process.argv[2]
-
-
-const url = "https://api.thecatapi.com/v1/breeds/search?q=" + breed
-
-
+const url = "https://api.thecatapi.com/v1/breeds/search?q="
 const request = require("request")
 
-request(url, function (error, response, body) {
-  console.error('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  const data = JSON.parse(body);
-  if (breed !== "American%20Bobtail" && (data[0].description === "American Bobtails are loving and incredibly intelligent cats possessing a distinctive wild appearance. They are extremely interactive cats that bond with their human family with great devotion.")) {
-    console.log("Ineligible breed")
-  }
-  else {
-    console.log(data[0].description);
-  }
-});
 
+const fetchBreedDescription = function (breed, callback) {
+  request(url + breed, function (error, response, body) {
+    //console.error('error:', error); // Print the error if one occurred
+    //console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+
+    const data = JSON.parse(body);
+    if (data.length === 0) {
+      //console.log("error")
+      process.exit()
+    }
+    else {
+      callback(error, data[0].description)
+    }
+    // if (breed !== "American%20Bobtail" && (data[0].description === "American Bobtails are loving and incredibly intelligent cats possessing a distinctive wild appearance. They are extremely interactive cats that bond with their human family with great devotion.")) {
+    //   console.log("Ineligible breed")
+    // }
+    // else {
+    //   console.log(data[0].description);
+    // }
+  });
+};
+
+module.exports = { fetchBreedDescription }
